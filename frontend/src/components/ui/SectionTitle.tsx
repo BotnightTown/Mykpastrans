1e5fb6002993ed44e4fa33390e3913c9657e94fb
BotnightTown <148649158+BotnightTown@users.noreply.github.com>
2026-03-15 19:27:36 +0200

    Update SectionTitle.tsx

--- frontend/src/components/ui/SectionTitle.tsx
+++ frontend/src/components/ui/SectionTitle.tsx
@@ -4,6 +4,6 @@ interface SectionTitleProps {
 
 export default function SectionTitle({ title }: SectionTitleProps) {
   return(
-    <h2 className="text-2xl md:text-4xl font-black text-(--primary-blue) border-l-4 md:border-l-6 border-(--accent-pink) pl-3 md:pl-4 mb-6 md:mb-8 uppercase"></h2>
+    <h2 className="text-2xl md:text-4xl font-black text-(--primary-blue) border-l-4 md:border-l-6 border-(--accent-pink) pl-3 md:pl-4 mb-6 md:mb-8 uppercase">{title}</h2>
   )
 }
